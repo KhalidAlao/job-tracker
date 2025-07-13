2cd3fd9333ad776d8941c039994a59019ce3d48f
KhalidAlao <alaokha@gmail.com>
2025-07-13 22:41:12 +0100

    Date Admission and Form Submission

--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ const jobList = document.getElementById("job-list");
 const template = document.getElementById("job-card-template");
 const addBtn = document.getElementById("add-btn");
 const applicationForm = document.getElementById("application-form");
+const jobTitle = document.getElementById("job-title");
+const companyName = document.getElementById("company-name");
+const jobStatus = document.getElementById("job-status");
+const dateAdded = document.getElementById("date-added");
+
+
+
+
 
 // Temporary test data
 jobs = [
@@ -15,15 +23,18 @@ jobs = [
     id: '1',
     title: 'Frontend Developer',
     company: 'Meta',
-    status: 'applied',
+    status: 'Applied',
+    dateAdded: 'today',
   },
   {
     id: '2',
     title: 'Backend Developer',
     company: 'Netflix',
-    status: 'interview',
+    status: 'Interview',
+    dateAdded: 'today',
   },
 ];
+
 
 
 function renderJobs() {
@@ -32,9 +43,11 @@ function renderJobs() {
   jobs.forEach((job) => {
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector('.job-id').textContent = job.id;
     clone.querySelector('.job-title').textContent = job.title;
     clone.querySelector('.company-name').textContent = job.company;
     clone.querySelector('.job-status').textContent = `Status: ${job.status}`;
+    clone.querySelector('.date-added').textContent = `Date Added: ${job.dateAdded}`;
 
     // Add custom data for later use (e.g., editing/deleting)
     clone.querySelector('.job-card').dataset.id = job.id;
@@ -51,6 +64,7 @@ function addJob(title, company, status) {
     title,
     company,
     status,
+    dateAdded: new Date().toLocaleDateString(),
   };
 
   jobs.push(job);
@@ -59,4 +73,22 @@ function addJob(title, company, status) {
 
 addBtn.addEventListener("click", function() {
   applicationForm.classList.toggle('hide');
-})
+});
+
+applicationForm.addEventListener("submit", function() {
+event.preventDefault();
+
+const title = jobTitle.value;
+const company = companyName.value;
+const status = jobStatus.value;
+
+if (title.trim() === "" || company.trim() === "" || status === "all") {
+  alert("Please fill in any empty fields!");
+
+}
+
+else {
+  addJob(title, company, status);
+}
+
+});
